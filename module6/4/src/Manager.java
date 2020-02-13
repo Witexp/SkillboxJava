d40eb5e4d0eaaab65ddffef74d96c9a37971a2dc
Witex <witexp@rambler.ru>
2020-02-13 19:09:37 +0300

    changed HomwWork_6.4 ver. 2

--- module6/4/src/Manager.java
+++ module6/4/src/Manager.java
@@ -1,16 +1,18 @@
 public class Manager extends Operator {
 
     private double earnSalary;
+    private double salary;
 
     public Manager (String name, double fixedSalary, int earnSalary)
     {
         super(name,fixedSalary);
         this.earnSalary = earnSalary;
+        this.salary = getFixedSalary() + (earnSalary/100 * 5);
     }
 
     @Override
     public double getMonthSalary() {
-        return getFixedSalary() + (earnSalary/100 * 5);
+        return this.salary;
     }
 }
 
--- module6/4/src/TopManager.java
+++ module6/4/src/TopManager.java
@@ -2,19 +2,22 @@ public class TopManager extends Operator implements Employee{
 
     private Company company;
     private double income;
+    private double salary;
     public TopManager(String name, double fixedSalary,Company company)
     {
         super(name,fixedSalary);
         this.company = company;
+        this.income = company.getIncome();
+        if (this.income > 10000000.00) {
+            this.salary = getFixedSalary() + (getFixedSalary()/100 * 150);
+        } else
+            this.salary = getFixedSalary();
     }
 
 
     @Override
     public double getMonthSalary() {
-        this.income = company.getIncome();
-       if (this.income > 10000000.00) {
-           return getFixedSalary() + (getFixedSalary()/100 * 150);
-       } else
-        return getFixedSalary() ;
+
+        return this.salary ;
     }
 }
