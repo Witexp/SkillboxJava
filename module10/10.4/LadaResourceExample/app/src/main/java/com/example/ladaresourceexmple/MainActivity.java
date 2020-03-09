ebfe9c924626da9352f92c4828f4a25ec9c7b6d9
Witex <witexp@rambler.ru>
2020-03-09 15:58:01 +0300

    Update Home Work 10.4 ver.2

--- module10/10.4/LadaResourceExample/app/src/main/java/com/example/ladaresourceexmple/MainActivity.java
+++ module10/10.4/LadaResourceExample/app/src/main/java/com/example/ladaresourceexmple/MainActivity.java
@@ -7,6 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
     static {
@@ -17,7 +23,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+        //TextView dateView = findViewById(R.id.date);
+        //TextView dateView2 = findViewById(R.id.date2);
+        Calendar calendar1 = Calendar.getInstance();
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.set(Calendar.HOUR_OF_DAY,12);
+        calendar2.set(Calendar.MINUTE,00);
+        Date date = calendar1.getTime();
+        Date date2 = calendar2.getTime();
+       //dateView.setText(date.toString());
+        //dateView2.setText(date2.toString());
+        if (date.after(date2)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
     }
 
     public void onChoseCareActivity(View view) {
